8914e3859e8e04b5bb3333f718923af0b4622e77
Steven Williams <evilgeniusnyc@me.com>
2020-07-15 13:49:38 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,4 +1,9 @@
 # Write your code here
-while (condition) do
+magic_exit_number = 0 
+count = 10 
+while count > 0 do 
+  break if count == magic_exit_number
+  puts #{count}
+  count = count - 1 
 end 
 
